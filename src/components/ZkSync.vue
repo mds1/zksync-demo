57c28922909e846c982dd7144d9d76b148892a2a
Matt Solomon <matt@mattsolomon.dev>
2020-08-10 17:31:43 -0700

    WIP: Add zksync package

--- src/components/ZkSync.vue
+++ src/components/ZkSync.vue
@@ -5,13 +5,28 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, onMounted } from '@vue/composition-api';
+import useAlerts from 'src/utils/alerts';
+import useWalletStore from 'src/store/wallet';
+
+function useZkSync() {
+  const { notifyUser, showError } = useAlerts();
+  const { signer, userAddress } = useWalletStore();
+
+  onMounted(async () => {
+    console.log(123123123);
+    const zksync = await import('zksync');
+    console.log(zksync);
+  });
+
+  return {};
+}
 
 export default defineComponent({
   name: 'zkSync',
 
   setup() {
-    return { /* */ };
+    return { ...useZkSync() };
   },
 });
 </script>
